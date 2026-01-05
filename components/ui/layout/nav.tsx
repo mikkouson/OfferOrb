d3bafc0c594dd4e3e981834f1a78a9569325a129
Leo Mikko Uson <usonleomikko@gmail.com>
2026-01-05 22:01:15 +0800

    feat: added navigation component

---
+++ components/ui/layout/nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <header className="flex h-16 items-center bg-accent-foreground/5">
+      <div className="mx-auto flex w-full max-w-7xl justify-between">
+        <h4>OfferOrb</h4>
+        <h4>AI-Powered Career Decision Assistant</h4>
+      </div>
+    </header>
+  );
+};
+
+export default Nav;
